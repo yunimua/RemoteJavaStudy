b3fe272c2e7a56992be0263a2474925675a6bfd7
yunimua <8401051208+yunimua@users.noreply.github.com>
2021-07-01 21:01:08 +0900

    기존내용 삭제

--- src/ClassB.java
+++
@@ -1,9 +0,0 @@
-
-public class ClassB {
-
-	public void methodA() {
-		System.out.println("111");
-		System.out.println("1112121");
-		return;
-	}
-}
--- src/myStudy/study01.java
+++
@@ -1,9 +0,0 @@
-package myStudy;
-
-public class study01 {
-
-	public static void main(String[] args) {
-		System.out.println("1234");
-		System.out.println("2222");
-	}
-}
